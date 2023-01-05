9e1d71bb5b80db2dd504674333923221da3fbc73
MONSTERMC <74623428+noradlb1@users.noreply.github.com>
2023-01-05 11:26:02 +0100

    CPU-RAM-Usage-Watcher-VB
    CPU-RAM-Usage-Watcher-VB

---
+++ utrack/utrack.Designer.vb
@@ -0,0 +1,164 @@
+﻿Namespace utrack
+	Partial Public Class utrack
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.components = New System.ComponentModel.Container()
+			Dim resources As New System.ComponentModel.ComponentResourceManager(GetType(utrack))
+			Me.show_cpu_usage = New System.Windows.Forms.Label()
+			Me.show_ram_usage = New System.Windows.Forms.Label()
+			Me.SystemTrayIcon = New System.Windows.Forms.NotifyIcon(Me.components)
+			Me.pictureBox = New System.Windows.Forms.PictureBox()
+			Me.label_cpu = New System.Windows.Forms.Label()
+			Me.label_ram = New System.Windows.Forms.Label()
+			Me.label_delay = New System.Windows.Forms.Label()
+			Me.trackBar = New System.Windows.Forms.TrackBar()
+			DirectCast(Me.pictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+			DirectCast(Me.trackBar, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' show_cpu_usage
+			' 
+			Me.show_cpu_usage.AutoSize = True
+			Me.show_cpu_usage.Font = New System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, (CByte(238)))
+			Me.show_cpu_usage.ForeColor = System.Drawing.Color.Cyan
+			Me.show_cpu_usage.Location = New System.Drawing.Point(121, 27)
+			Me.show_cpu_usage.Name = "show_cpu_usage"
+			Me.show_cpu_usage.Size = New System.Drawing.Size(28, 15)
+			Me.show_cpu_usage.TabIndex = 0
+			Me.show_cpu_usage.Text = "..."
+			' 
+			' show_ram_usage
+			' 
+			Me.show_ram_usage.AutoSize = True
+			Me.show_ram_usage.Font = New System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, (CByte(238)))
+			Me.show_ram_usage.ForeColor = System.Drawing.Color.Lime
+			Me.show_ram_usage.Location = New System.Drawing.Point(121, 42)
+			Me.show_ram_usage.Name = "show_ram_usage"
+			Me.show_ram_usage.Size = New System.Drawing.Size(28, 15)
+			Me.show_ram_usage.TabIndex = 1
+			Me.show_ram_usage.Text = "..."
+			' 
+			' SystemTrayIcon
+			' 
+			Me.SystemTrayIcon.Text = "..."
+			Me.SystemTrayIcon.Visible = True
+'			Me.SystemTrayIcon.MouseClick += New System.Windows.Forms.MouseEventHandler(Me.SystemTrayIcon_MouseClick)
+			' 
+			' pictureBox
+			' 
+			Me.pictureBox.BackColor = System.Drawing.Color.FromArgb((CInt((CByte(10)))), (CInt((CByte(10)))), (CInt((CByte(10)))))
+			Me.pictureBox.Location = New System.Drawing.Point(15, 27)
+			Me.pictureBox.Name = "pictureBox"
+			Me.pictureBox.Size = New System.Drawing.Size(100, 100)
+			Me.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+			Me.pictureBox.TabIndex = 6
+			Me.pictureBox.TabStop = False
+			' 
+			' label_cpu
+			' 
+			Me.label_cpu.AutoSize = True
+			Me.label_cpu.Font = New System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, (CByte(238)))
+			Me.label_cpu.ForeColor = System.Drawing.Color.Cyan
+			Me.label_cpu.Location = New System.Drawing.Point(12, 9)
+			Me.label_cpu.Name = "label_cpu"
+			Me.label_cpu.Size = New System.Drawing.Size(28, 15)
+			Me.label_cpu.TabIndex = 7
+			Me.label_cpu.Text = "CPU"
+			' 
+			' label_ram
+			' 
+			Me.label_ram.AutoSize = True
+			Me.label_ram.Font = New System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, (CByte(238)))
+			Me.label_ram.ForeColor = System.Drawing.Color.Lime
+			Me.label_ram.Location = New System.Drawing.Point(87, 9)
+			Me.label_ram.Name = "label_ram"
+			Me.label_ram.Size = New System.Drawing.Size(28, 15)
+			Me.label_ram.TabIndex = 8
+			Me.label_ram.Text = "RAM"
+			' 
+			' label_delay
+			' 
+			Me.label_delay.Anchor = (CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles))
+			Me.label_delay.AutoSize = True
+			Me.label_delay.Location = New System.Drawing.Point(12, 189)
+			Me.label_delay.Name = "label_delay"
+			Me.label_delay.Size = New System.Drawing.Size(85, 13)
+			Me.label_delay.TabIndex = 10
+			Me.label_delay.Text = "Delay: ... ms"
+			' 
+			' trackBar
+			' 
+			Me.trackBar.Anchor = (CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
+			Me.trackBar.LargeChange = 100
+			Me.trackBar.Location = New System.Drawing.Point(15, 141)
+			Me.trackBar.Maximum = 3000
+			Me.trackBar.Minimum = 50
+			Me.trackBar.Name = "trackBar"
+			Me.trackBar.Size = New System.Drawing.Size(457, 45)
+			Me.trackBar.TabIndex = 14
+			Me.trackBar.TickStyle = System.Windows.Forms.TickStyle.TopLeft
+			Me.trackBar.Value = 200
+'			Me.trackBar.ValueChanged += New System.EventHandler(Me.trackBar_ValueChanged)
+			' 
+			' utrack
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.BackColor = System.Drawing.Color.FromArgb((CInt((CByte(15)))), (CInt((CByte(15)))), (CInt((CByte(15)))))
+			Me.ClientSize = New System.Drawing.Size(484, 211)
+			Me.Controls.Add(Me.trackBar)
+			Me.Controls.Add(Me.label_delay)
+			Me.Controls.Add(Me.label_ram)
+			Me.Controls.Add(Me.label_cpu)
+			Me.Controls.Add(Me.pictureBox)
+			Me.Controls.Add(Me.show_ram_usage)
+			Me.Controls.Add(Me.show_cpu_usage)
+			Me.Font = New System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, (CByte(238)))
+			Me.ForeColor = System.Drawing.Color.DimGray
+			Me.Icon = (DirectCast(resources.GetObject("$this.Icon"), System.Drawing.Icon))
+			Me.MinimumSize = New System.Drawing.Size(500, 250)
+			Me.Name = "utrack"
+			Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+			Me.Text = "CPU & RAM Watcher"
+'			Me.Load += New System.EventHandler(Me.utrack_Load)
+			DirectCast(Me.pictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+			DirectCast(Me.trackBar, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+			Me.PerformLayout()
+
+		End Sub
+
+		#End Region
+
+		Private show_cpu_usage As System.Windows.Forms.Label
+		Private show_ram_usage As System.Windows.Forms.Label
+		Private WithEvents SystemTrayIcon As System.Windows.Forms.NotifyIcon
+		Private pictureBox As System.Windows.Forms.PictureBox
+		Private label_cpu As System.Windows.Forms.Label
+		Private label_ram As System.Windows.Forms.Label
+		Private label_delay As System.Windows.Forms.Label
+		Private WithEvents trackBar As System.Windows.Forms.TrackBar
+	End Class
+End Namespace
+
---
+++ utrack/utrack.vb
@@ -0,0 +1,123 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Windows.Forms
+Imports System.Diagnostics
+Imports System.Threading
+Imports System.Management
+Imports System.Runtime.InteropServices
+
+Namespace utrack
+	Partial Public Class utrack
+		Inherits Form
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub utrack_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+			startMonitorThread()
+			updateValue()
+		End Sub
+
+		Private Sub startMonitorThread()
+			CType(New Thread(AddressOf Monitor) With {.IsBackground = True}, Thread).Start()
+		End Sub
+
+		Private Shared quit As Boolean = False
+		Private Sub Monitor()
+			Try
+
+				Dim cpuCounter As New PerformanceCounter("Processor", "% Processor Time", "_Total")
+
+				Do While Not quit
+					Dim cpu As Double = Math.Round(cpuCounter.NextValue(), 2)
+					Dim ram As Double = 0
+
+					Dim objectQuery As New ObjectQuery("SELECT * FROM Win32_OperatingSystem")
+					Dim managementObjectSearcher As New ManagementObjectSearcher(objectQuery)
+					Dim managementObjectCollection As ManagementObjectCollection = managementObjectSearcher.Get()
+					For Each managementObject As ManagementObject In managementObjectCollection
+						Dim tot As Double = Double.Parse(managementObject("TotalVisibleMemorySize").ToString()) * 1024
+						Dim free As Double = Double.Parse(managementObject("FreePhysicalMemory").ToString()) * 1024
+						Dim virttot As Double = Double.Parse(managementObject("TotalVirtualMemorySize").ToString()) * 1024
+						Dim virtfree As Double = Double.Parse(managementObject("FreeVirtualMemory").ToString()) * 1024
+
+						ram = Math.Round(100 - free / tot * 100, 2)
+
+						show_ram_usage.Text = "RAM USAGE: " & ram & "%" & Environment.NewLine & "|- Total Visible Memory.: " & ROund(tot) & " (" & tot & "B)" & Environment.NewLine & "|- Free Physical Memory.: " & ROund(free) & " (" & free & " B)" & Environment.NewLine & "|- Total Virtual Memory.: " & ROund(virttot) & " (" & virttot & " B)" & Environment.NewLine & "+- Free Virtual Memory..: " & ROund(virtfree) & " (" & virtfree & " B)"
+					Next managementObject
+
+					'print
+					show_cpu_usage.Text = "CPU USAGE: " & cpu & "%"
+					'show_ram_usage.Text = "RAM: " + Math.Round(ram, 2) + "MB";
+					SystemTrayIcon.Text = "CPU: " & cpu.ToString() & "%" & Environment.NewLine & "RAM: " & ram.ToString() & "%"
+
+					Dim bmp As Bitmap = draw_image(cpu, ram)
+
+					'convert and show on icon
+					SystemTrayIcon.Icon = Icon.FromHandle(bmp.GetHicon())
+
+					pictureBox.Image = bmp
+
+					Thread.Sleep(trackBar.Value)
+				Loop
+			Catch e As Exception
+				Console.Error.WriteLine(e.Message)
+				startMonitorThread()
+			End Try
+		End Sub
+
+		Private Function draw_image(ByVal cpu_usage As Double, ByVal ram_usage As Double) As Bitmap
+			Dim image As New Bitmap(100, 100)
+
+			Using g As Graphics = Graphics.FromImage(image)
+				g.Clear(Color.FromArgb(10, 10, 10))
+				g.FillRectangle(Brushes.Cyan, New Rectangle(0, 0, image.Width \ 2 - 5, CInt(Math.Truncate(cpu_usage))))
+				g.FillRectangle(Brushes.Lime, New Rectangle(image.Width \ 2 + 5, 0, image.Width \ 2 + 5, CInt(Math.Truncate(ram_usage))))
+
+				Dim rect As New Rectangle(0, 0, 100, 100)
+				Dim penWidth As Integer = 5
+				Using pen As New Pen(Color.Red, penWidth)
+					Dim shrinkAmount As Single = pen.Width \ 2
+					g.DrawRectangle(pen, rect.X + shrinkAmount, rect.Y + shrinkAmount, rect.Width - penWidth, rect.Height - penWidth) ' shrink height with one pen-width -  shrink width with one pen-width -  move half a pen-width to the down -  move half a pen-width to the right
+				End Using
+			End Using
+			'flip vert
+			image.RotateFlip(RotateFlipType.Rotate180FlipX)
+			Return image
+		End Function
+
+		Private Sub SystemTrayIcon_MouseClick(ByVal sender As Object, ByVal e As MouseEventArgs) Handles SystemTrayIcon.MouseClick
+			Select Case e.Button
+				Case MouseButtons.Left
+					Me.Visible = Not Me.Visible
+				Case MouseButtons.Right
+					Environment.Exit(0)
+			End Select
+		End Sub
+
+		Private Sub trackBar_ValueChanged(ByVal sender As Object, ByVal e As EventArgs) Handles trackBar.ValueChanged
+			updateValue()
+		End Sub
+
+		Private Sub updateValue()
+			label_delay.Text = "Delay: " & trackBar.Value & " ms"
+		End Sub
+
+		Public Shared Function ROund(ByVal len As Double) As String
+			Dim sizes() As String = { "B", "KB", "MB", "GB", "TB" }
+			Dim order As Integer = 0
+			Do While len >= 1024 AndAlso order < sizes.Length - 1
+				order += 1
+				len = len / 1024
+			Loop
+			Return String.Format("{0:0.##} {1}", len, sizes(order))
+		End Function
+	End Class
+End Namespace
